ebcef666af00ef4fac4fe5e082d58ee12bdf4817
katmilne <katmilne39@gmail.com>
2024-03-24 17:01:35 +0000

    Initial commit and added sample test

---
+++ src/test/java/com/katmilne/cashcard/CashCardJsonTest.java
@@ -0,0 +1,13 @@
+package com.katmilne.cashcard;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CashCardJsonTest {
+
+    @Test
+    void myFirstTest() {
+        assertThat(1).isEqualTo(1);
+    }
+}
