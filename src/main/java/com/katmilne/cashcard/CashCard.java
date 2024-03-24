07470ac18e31866d7f162160ea1a2b83116a146d
katmilne <katmilne39@gmail.com>
2024-03-24 17:37:46 +0000

    Implemented TDD for CashCard class, Implemented CashCard class and expected json example file. All tests pass.

---
+++ src/main/java/com/katmilne/cashcard/CashCard.java
@@ -0,0 +1,13 @@
+package com.katmilne.cashcard;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CashCard {
+
+    private Long id;
+    private Double amount;
+
+}
